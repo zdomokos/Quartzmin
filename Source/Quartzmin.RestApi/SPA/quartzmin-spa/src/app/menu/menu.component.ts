b0f5a02072c250581adde2580516cde4926f479a
David Klingenberg <dklingenberg@gmail.com>
2019-07-24 07:58:35 -0400

    Add menu and footer

---
+++ Source/Quartzmin.RestApi/SPA/quartzmin-spa/src/app/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  menuItems = [
+    { name: 'Overview', route: '/' },
+    { name: 'Jobs', route: 'jobs' },
+    { name: 'Triggers', route: 'triggers' },
+    { name: 'Executions', route: 'executions' },
+    { name: 'History', route: 'history' },
+    { name: 'Calendars', route: 'calendars' }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
