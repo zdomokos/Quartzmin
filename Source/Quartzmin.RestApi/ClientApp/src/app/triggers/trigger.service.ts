b63869299213c13e21dd5e0e8a6326e0fc56a778
David Klingenberg <dklingenberg@gmail.com>
2019-07-25 00:36:47 +0200

    Update triggers

--- Source/Quartzmin.RestApi/ClientApp/src/app/triggers/trigger.service.ts
+++ Source/Quartzmin.RestApi/ClientApp/src/app/triggers/trigger.service.ts
@@ -1,27 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
 import { Trigger } from '../model/trigger';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TriggerService {
+  static readonly API_ROOT_URL = `${environment.apiRoot}/triggers`;
+  static readonly DETAIL_URL = `${TriggerService.API_ROOT_URL}/{triggerId}`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
 
   getAll(): Observable<Trigger[]> {
-
+    return this.http.get<Trigger[]>(TriggerService.API_ROOT_URL);
   }
 
   getDetail(): Observable<Trigger> {
+    return null;
 
   }
 
   create(trigger: Trigger): Observable<Trigger> {
+    return null;
 
   }
 
   update(trigger: Trigger): Observable<Trigger> {
-
+    return null;
   }
 }
