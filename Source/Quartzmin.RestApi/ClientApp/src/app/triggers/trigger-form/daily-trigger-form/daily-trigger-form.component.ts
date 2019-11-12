c36b1e5aade88445c04e25cf5ac49fd6fbaef357
David Klingenberg <dklingenberg@gmail.com>
2019-11-12 11:35:47 +0100

    Update daily trigger form - generate days, add input types

--- Source/Quartzmin.RestApi/ClientApp/src/app/triggers/trigger-form/daily-trigger-form/daily-trigger-form.component.ts
+++ Source/Quartzmin.RestApi/ClientApp/src/app/triggers/trigger-form/daily-trigger-form/daily-trigger-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { getLocaleFirstDayOfWeek } from '@angular/common';
+
+interface DayOfWeek {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-daily-trigger-form',
@@ -9,10 +15,19 @@ import { FormGroup } from '@angular/forms';
 export class DailyTriggerFormComponent implements OnInit {
   @Input()
   formGroup: FormGroup;
+  daysOfWeek = this.getDaysOfWeek();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  getDaysOfWeek(): DayOfWeek[] {
+    const locale = navigator.language;
+
+    return Array.from({ length: 7 }).map((_, index) => ({
+      id: index,
+      name: new Date(1970, 1 - 1, index + 5).toLocaleString(locale, { weekday: 'long' })
+    }));
+  }
 }
